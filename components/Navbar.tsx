88c6782c5b1e1ec9d4325ce9c9a4c3b3cf54662f
faizan <faizanzaid789@gmail.com>
2022-10-30 16:43:08 +0530

    added authentication proper authenticated system

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React, { useContext, useEffect, useState } from "react";
 import { userContext } from "../context/userContext";
 
@@ -8,20 +9,21 @@ type Props = {
 };
 
 const Navbar = ({ authState }: Props) => {
-  const {isAuthenticated}=useContext(userContext)
-  const [admin, setAdmin]=useState<boolean>(false)
-  useEffect(()=>{
-    if(isAuthenticated()){
-         setAdmin(true)
+  const { isAuthenticated } = useContext(userContext);
+  const [admin, setAdmin] = useState<boolean>(false);
+  const router = useRouter();
+  useEffect(() => {
+    if (isAuthenticated()) {
+      setAdmin(true);
+    } else {
+      setAdmin(false);
     }
-    else{
-      setAdmin(false)
-    }
+  }, [router.query]);
 
-  },[])
-  const logOut=()=>{
-    localStorage.removeItem('auth')
-  }
+  const logOut = () => {
+    router.push("/")
+    localStorage.removeItem("auth");
+  };
   return (
     <header className="primary-bg sticky top-0 backdrop-blur-md z-20 shadow-md">
       <nav className="flex justify-between p-2 h-[59px] items-center max-w-[1430px] mx-auto">
@@ -49,14 +51,19 @@ const Navbar = ({ authState }: Props) => {
         </div>
         {authState ? (
           <div className="flex items-center space-x-5">
-          <p className="commonButton font-semibold cursor-pointer text-white px-5 py-1" onClick={logOut}>
-            Logout
-          </p>
-      {admin && ( <p className="commonButton font-semibold cursor-pointer text-white px-3 py-1">
-            Welcome Admin
-          </p>)}
-       </div>
-      
+            <p
+              className="commonButton font-semibold cursor-pointer text-white px-5 py-1"
+              onClick={logOut}
+            >
+              Logout
+            </p>
+            {admin && (
+              <Link href={"/admin"} ><p className="commonButton font-semibold cursor-pointer text-white px-3 py-1">
+                Welcome Admin
+              </p>
+              </Link>
+            )}
+          </div>
         ) : (
           <div className="flex items-center space-x-12">
             <Link href={"/login"}>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,29 @@
 import "../styles/globals.css";
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 import type { AppProps } from "next/app";
 import NextNProgress from "nextjs-progressbar";
 import { useEffect, useState } from "react";
 import { AppProvider } from "../context/userContext";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [authState, setAuthState] = useState<boolean | undefined>();
-  const [key, setKey] = useState<number>();
+  const [authState, setAuthState] = useState<boolean>();
+  const router=useRouter();
+  const [key, setKey] = useState<number>(90);
+
   useEffect(() => {
-    if (localStorage.getItem("auth")) {
-      const auth = JSON.parse(localStorage.getItem("auth")!);
-      if (auth.success && auth.authToken) setAuthState(true);
-      else setAuthState(false);
+    const auth = JSON.parse(localStorage.getItem("auth")!);
+    if (auth?.success && auth?.authToken) {
+      setAuthState(true);
     }
-    setKey(Math.random());
-  }, []);
+    else{
+      setAuthState(false)
+    }
+  }, [router.query]);
+
   return (
     <>
       <AppProvider>
@@ -29,9 +34,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           height={3}
           showOnShallow={true}
         />
-        <Navbar key={key} authState={authState} />
+        <Navbar  authState={authState} />
         <Component {...pageProps} />
-        <Footer/>
+        <Footer />
         <ToastContainer />
       </AppProvider>
     </>
