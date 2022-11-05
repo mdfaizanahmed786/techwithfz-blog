4dd69eff853d03cc53e744bcb04b97f05f6fe77c
faizan <faizanzaid789@gmail.com>
2022-11-05 12:57:47 +0530

    added some modifications in forgot page.

--- pages/forgot.tsx
+++ pages/forgot.tsx
@@ -2,19 +2,65 @@ import Head from "next/head";
 import React, { useRef, useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import { toast } from "react-toastify";
+import { useSession } from "next-auth/react";
+import { getProviders, signIn } from "next-auth/react";
 
-type Props = {};
-const token=process.env.NEXT_PUBLIC_FORGOT_TOKEN
+interface Auth {
+  callbackUrl: string;
+  id: string;
+  name: string;
+  signinUrl: string;
+  type: string;
+}
+interface GoogleAuth {
+  google: Auth;
+}
+type Props = {
+  authState: boolean;
+  providers: GoogleAuth;
+};
+
+const token = process.env.NEXT_PUBLIC_FORGOT_TOKEN;
 const Forgot = (props: Props) => {
-  
-    console.log(token)
   const emailRef = useRef<HTMLInputElement>(null);
-  const [captcha, setCaptcha]=useState<string| null>("")
+  const [captcha, setCaptcha] = useState<string | null>("");
+  const { data } = useSession();
+
+  const session: any = data;
   const handleSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault();
-    try{
-        if(!captcha){
-          toast.error('Invalid captcha', {
+    try {
+      if (!captcha) {
+        toast.error("Invalid captcha", {
+          position: "top-right",
+          autoClose: 1800,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+        return;
+      }
+      const forgotPassword = await fetch(
+        "https://techwithfz.vercel.app/api/forgot",
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify({
+            token,
+            email: emailRef.current?.value,
+          }),
+        }
+      );
+      const response = await forgotPassword.json();
+      if (response.success && captcha) {
+        toast.success(
+          "Instructions have been sent to your email to reset your password!",
+          {
             position: "top-right",
             autoClose: 1800,
             hideProgressBar: false,
@@ -23,23 +69,10 @@ const Forgot = (props: Props) => {
             draggable: true,
             progress: undefined,
             theme: "dark",
-            });
-          return;
-        }
-      const forgotPassword = await fetch("https://techwithfz.vercel.app/api/forgot", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify({
-            token,
-          email: emailRef.current?.value,
-          
-        }),
-      });
-      const response=await forgotPassword.json();
-      if(response.success && captcha){
-        toast.success('Instructions have been sent to your email to reset your password!', {
+          }
+        );
+      } else {
+        toast.error("User with this email do not exist!", {
           position: "top-right",
           autoClose: 1800,
           hideProgressBar: false,
@@ -48,24 +81,10 @@ const Forgot = (props: Props) => {
           draggable: true,
           progress: undefined,
           theme: "dark",
-          });
-    
+        });
       }
-      else{
-        toast.error('User with this email do not exist!', {
-          position: "top-right",
-          autoClose: 1800,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "dark",
-          });
-      }
-    }
-    catch(er){
-      toast.error('Server error!', {
+    } catch (er) {
+      toast.error("Server error!", {
         position: "top-right",
         autoClose: 1800,
         hideProgressBar: false,
@@ -74,70 +93,79 @@ const Forgot = (props: Props) => {
         draggable: true,
         progress: undefined,
         theme: "dark",
-        });
+      });
     }
-    
   };
-  function onChange(value:string|null) {
-    setCaptcha(value)
+  function onChange(value: string | null) {
+    setCaptcha(value);
   }
-  
+
   return (
     <div className="loginPage min-h-screen md:items-center md:flex">
-    <Head>
-      <title>Reset your password</title>
-    </Head>
-    <div className="flex md:max-w-[1450px] md:mx-auto mx-5 md:py-5">
-    
+      <Head>
+        <title>Reset your password</title>
+      </Head>
+      <div className="flex md:max-w-[1450px] md:mx-auto mx-5 md:py-5">
         <img
           src="/forgot.png"
           className="object-cover w-72 rounded-tl-md rounded-bl-md hidden md:inline-flex"
           alt="login_image"
         />
-     
-      <div className="bg-[#1E1E1E] md:px-7 px-5 rounded-md md:rounded-none md:rounded-tr-md md:rounded-br-md flex flex-col gap-8  items-center  justify-center shadow-lg  w-full py-7 mt-28 md:mt-0 md:py-6 mb-12 md:mb-0 flex-1">
-        <form onSubmit={handleSubmit} className="flex flex-col gap-7 w-full">
+
+        <div className="bg-[#1E1E1E] md:px-7 px-5 rounded-md md:rounded-none md:rounded-tr-md md:rounded-br-md flex flex-col gap-8  items-center  justify-center shadow-lg  w-full py-7 mt-28 md:mt-0 md:py-6 mb-12 md:mb-0 flex-1">
+          <form onSubmit={handleSubmit} className="flex flex-col gap-7 w-full">
             <h2 className="font-bold text-2xl text-center text-white items-end">
-             Forgot Password?
+              Forgot Password?
             </h2>
-      
-            
-          
-          <div className="flex flex-col gap-1">
-            <label htmlFor="email" className="font-semibold text-white">
-              Email
-            </label>
-            <input
-              ref={emailRef}
-              type="email"
-              name="email"
-              id="email"
-              minLength={7}
-              autoComplete="false"
-              className="bg-[#2E2E2E] px-5 py-2 rounded-md outline-none text-white border-[#10935F] border-2 "
-              required
-              placeholder="Enter your email"
-              pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$"
-            />
-          </div>
-          <div className='flex justify-start w-full'>
-        <ReCAPTCHA
-      sitekey="6LcGfNYiAAAAALXVdk9psJDgpo_nUEb6D5RdqW7T"
-   
-    onChange={onChange}
-  />
-  </div>
-          
-          <button type="submit" className="commonButton py-2 font-semibold text-white" >Continue</button>
-        </form>
-        <p className="text-white text-sm">
-           You'll receive an email from us to reset your password.
+
+            <div className="flex flex-col gap-1">
+              <label htmlFor="email" className="font-semibold text-white">
+                Email
+              </label>
+              <input
+                ref={emailRef}
+                type="email"
+                name="email"
+                id="email"
+                minLength={7}
+                autoComplete="false"
+                className="bg-[#2E2E2E] px-5 py-2 rounded-md outline-none text-white border-[#10935F] border-2 "
+                required
+                placeholder="Enter your email"
+                pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$"
+              />
+            </div>
+            <div className="flex justify-start w-full">
+              <ReCAPTCHA
+                sitekey="6LcGfNYiAAAAALXVdk9psJDgpo_nUEb6D5RdqW7T"
+                onChange={onChange}
+              />
+            </div>
+
+            <button
+              type="submit"
+              className={`${
+                session?.user || props.authState
+                  ? "bg-gray-500 text-white py-2 font-semibold rounded-md"
+                  : "commonButton py-2 font-semibold text-white"
+              } `}
+              disabled={session?.user || props.authState}
+            >
+              Login
+            </button>
+          </form>
+          <p className="text-white text-sm">
+            You'll receive an email from us to reset your password.
           </p>
-        
+        </div>
       </div>
     </div>
-  </div>
   );
 };
-
+export async function getServerSideProps(context: object) {
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
+}
 export default Forgot;
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,117 +1,41 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import React, { useRef, useState } from 'react'
-import { toast } from 'react-toastify';
-import { useSession, signOut } from "next-auth/react"
-import { getProviders, signIn } from "next-auth/react"
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useRef, useState } from "react";
+import { toast } from "react-toastify";
+import { useSession } from "next-auth/react";
+import { getProviders, signIn } from "next-auth/react";
 import ReCAPTCHA from "react-google-recaptcha";
 
-
-interface Auth{
-  
-  callbackUrl:string
-  id:string
-  name:string
-  signinUrl:string
-  type:string
+interface Auth {
+  callbackUrl: string;
+  id: string;
+  name: string;
+  signinUrl: string;
+  type: string;
 }
-interface GoogleAuth{
-  google:Auth
+interface GoogleAuth {
+  google: Auth;
 }
 type Props = {
-  authState:boolean
-  providers:GoogleAuth
-}
-
+  authState: boolean;
+  providers: GoogleAuth;
+};
 
 const Login = (props: Props) => {
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
-  const [captcha, setCaptcha]=useState<string| null>("")
-  const router=useRouter();
-  const {data}=useSession();
- 
-  const session:any=data
-  const loginWithGoogle:any=signIn;
+  const [captcha, setCaptcha] = useState<string | null>("");
+  const router = useRouter();
+  const { data } = useSession();
 
-const authSignin=()=>{
+  const session: any = data;
+  const loginWithGoogle: any = signIn;
 
-  loginWithGoogle(props.providers.google.id)
-if(session)
-toast.success('Login Success!', {
-  position: "top-right",
-  autoClose: 1800,
-  hideProgressBar: false,
-  closeOnClick: true,
-  pauseOnHover: true,
-  draggable: true,
-  progress: undefined,
-  theme: "dark",
-  });
-
-}
-  const handleSubmit = async (e: React.SyntheticEvent) => {
-    
-    e.preventDefault();
-    try{
-      
-      if(!captcha){
-        toast.error('Invalid captcha', {
-          position: "top-right",
-          autoClose: 1800,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "dark",
-          });
-        return;
-      }
-      const getUser = await fetch("https://techwithfz.vercel.app/api/getuser", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify({
-    
-          email: emailRef.current?.value,
-          password: passwordRef.current?.value,
-        }),
-      });
-      const response = await getUser.json();
-      if(response.success && captcha){
-        toast.success('Login Success!', {
-          position: "top-right",
-          autoClose: 1800,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "dark",
-          });
-          router.push("/")
-          localStorage.setItem("auth", JSON.stringify(response));
-      }
-      else{
-        toast.error('Check your credentials!', {
-          position: "top-right",
-          autoClose: 1800,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "dark",
-          });
-
-      }
-
-    }
-    catch(er){
-      toast.error('Please try to login again with valid credentials!', {
+  const authSignin = () => {
+    loginWithGoogle(props.providers.google.id);
+    if (session)
+      toast.success("Login Success!", {
         position: "top-right",
         autoClose: 1800,
         hideProgressBar: false,
@@ -120,88 +44,183 @@ toast.success('Login Success!', {
         draggable: true,
         progress: undefined,
         theme: "dark",
-        });
-    }
-
+      });
   };
-  function onChange(value:string|null) {
-    setCaptcha(value)
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    try {
+      if (!captcha) {
+        toast.error("Invalid captcha", {
+          position: "top-right",
+          autoClose: 1800,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+        return;
+      }
+      const getUser = await fetch("https://techwithfz.vercel.app/api/getuser", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          email: emailRef.current?.value,
+          password: passwordRef.current?.value,
+        }),
+      });
+      const response = await getUser.json();
+      if (response.success && captcha) {
+        toast.success("Login Success!", {
+          position: "top-right",
+          autoClose: 1800,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+        router.push("/");
+        localStorage.setItem("auth", JSON.stringify(response));
+      } else {
+        toast.error("Check your credentials!", {
+          position: "top-right",
+          autoClose: 1800,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+      }
+    } catch (er) {
+      toast.error("Please try to login again with valid credentials!", {
+        position: "top-right",
+        autoClose: 1800,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    }
+  };
+  function onChange(value: string | null) {
+    setCaptcha(value);
   }
-  
+
   return (
-    <div className='loginPage min-h-screen md:items-center  md:flex'>
+    <div className="loginPage min-h-screen md:items-center  md:flex">
       <Head>
-  <title>Login | techWithFZ</title>
+        <title>Login | techWithFZ</title>
       </Head>
-      <div className='flex md:max-w-[1450px] md:mx-auto mx-5 '>
-      <div className='imageContainer md:flex-[0.45] hidden md:inline-flex'>
-        <img src="/login.png" className='object-cover h-full rounded-tl-md rounded-bl-md' alt="login_image" />
-
-      </div>
-       <div className='bg-[#1E1E1E] md:px-7 px-5 rounded-md md:rounded-none md:rounded-tr-md md:rounded-br-md flex flex-col gap-8  items-center md:flex-[0.55] justify-center shadow-lg  w-full py-7 mt-16 md:mt-0 md:py-0 mb-12 md:mb-0'>
-
-      <form onSubmit={handleSubmit} className="flex flex-col gap-7 w-full">
-        <h2 className='font-bold text-2xl text-center text-white'>Login</h2>
-        <div className='google bg-white flex gap-7 text-black justify-center items-center rounded-md py-1 px-2 cursor-pointer border-blue-500 border-2' onClick={authSignin}>
-          <img src="https://developers.google.com/identity/images/g-logo.png" alt="google_logo" className='h-10 w-10' />
-             <p className='text-lg'>Login in with {props.providers.google.name}</p>
+      <div className="flex md:max-w-[1450px] md:mx-auto mx-5 ">
+        <div className="imageContainer md:flex-[0.45] hidden md:inline-flex">
+          <img
+            src="/login.png"
+            className="object-cover h-full rounded-tl-md rounded-bl-md"
+            alt="login_image"
+          />
         </div>
-        
-        <div className='flex flex-col gap-1'>
+        <div className="bg-[#1E1E1E] md:px-7 px-5 rounded-md md:rounded-none md:rounded-tr-md md:rounded-br-md flex flex-col gap-8  items-center md:flex-[0.55] justify-center shadow-lg  w-full py-7 mt-16 md:mt-0 md:py-0 mb-12 md:mb-0">
+          <form onSubmit={handleSubmit} className="flex flex-col gap-7 w-full">
+            <h2 className="font-bold text-2xl text-center text-white">Login</h2>
+            <div
+              className="google bg-white flex gap-7 text-black justify-center items-center rounded-md py-1 px-2 cursor-pointer border-blue-500 border-2"
+              onClick={authSignin}
+            >
+              <img
+                src="https://developers.google.com/identity/images/g-logo.png"
+                alt="google_logo"
+                className="h-10 w-10"
+              />
+              <p className="text-lg">
+                Login in with {props.providers.google.name}
+              </p>
+            </div>
 
-        <label htmlFor="email" className='font-semibold text-white'>Email</label>
-        <input
-          ref={emailRef}
-          type="email"
-          name="email"
-          id="email"
-          minLength={7}
-          autoComplete="false"
-          className="bg-[#2E2E2E] px-5 py-2 rounded-md outline-none text-white  border-[#10935F] border-2"
-          required
-          pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$"
-          placeholder='Enter your email'
-        />
+            <div className="flex flex-col gap-1">
+              <label htmlFor="email" className="font-semibold text-white">
+                Email
+              </label>
+              <input
+                ref={emailRef}
+                type="email"
+                name="email"
+                id="email"
+                minLength={7}
+                autoComplete="false"
+                className="bg-[#2E2E2E] px-5 py-2 rounded-md outline-none text-white  border-[#10935F] border-2"
+                required
+                pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$"
+                placeholder="Enter your email"
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="password" className="font-semibold text-white">
+                Password
+              </label>
+              <input
+                ref={passwordRef}
+                type="password"
+                name="password"
+                id="password"
+                autoComplete="false"
+                minLength={5}
+                className="bg-[#2E2E2E] px-2 py-2 rounded-md outline-none  text-white border-[#10935F] border-2"
+                required
+                placeholder="Enter your password"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <p className="text-white text-center">
+                Forgot your password?{" "}
+                <Link href="/forgot">
+                  <span className="textStyle font-bold cursor-pointer">
+                    Reset Password
+                  </span>
+                </Link>
+              </p>
+            </div>
+            <div className="flex justify-start w-full">
+              <ReCAPTCHA
+                sitekey="6LcGfNYiAAAAALXVdk9psJDgpo_nUEb6D5RdqW7T"
+                onChange={onChange}
+              />
+            </div>
+            <button
+              type="submit"
+              className={`${
+                session?.user || props.authState
+                  ? "bg-gray-500 text-white py-2 font-semibold rounded-md"
+                  : "commonButton py-2 font-semibold text-white"
+              } `}
+              disabled={session?.user || props.authState}
+            >
+              Login
+            </button>
+          </form>
+          <p className="text-white text-sm">
+            Not a registered user?{" "}
+            <Link href="/signup">
+              <span className="textStyle font-bold cursor-pointer">Signup</span>
+            </Link>
+          </p>
         </div>
-        <div className='flex flex-col gap-1'>
-          
-        <label htmlFor="password" className='font-semibold text-white'>Password</label>
-        <input
-          ref={passwordRef}
-          type="password"
-          name="password"
-          id="password"
-          autoComplete="false"
-          minLength={5}
-          className="bg-[#2E2E2E] px-2 py-2 rounded-md outline-none  text-white border-[#10935F] border-2"
-          required
-          placeholder='Enter your password'
-        />
-        </div>
-        <div className='flex flex-col gap-2'>
-          <p className='text-white text-center'>Forgot your password? <Link href="/forgot"><span className='textStyle font-bold cursor-pointer'>Reset Password</span></Link></p>
-
-        </div>
-        <div className='flex justify-start w-full'>
-        <ReCAPTCHA
-    sitekey="6LcGfNYiAAAAALXVdk9psJDgpo_nUEb6D5RdqW7T"
-   
-    onChange={onChange}
-  />
-
-        </div>
-        <button type="submit" className={`${session?.user || props.authState ?  "bg-gray-500 text-white py-2 font-semibold rounded-md" : "commonButton py-2 font-semibold text-white"} `} disabled={session?.user || props.authState}>Login</button>
-      </form>
-      <p className='text-white text-sm'>Not a registered user? <Link href="/signup"><span className='textStyle font-bold cursor-pointer'>Signup</span></Link></p>
-       </div>
       </div>
     </div>
   );
-}
-export async function getServerSideProps(context:object) {
-  const providers = await getProviders()
+};
+export async function getServerSideProps(context: object) {
+  const providers = await getProviders();
   return {
     props: { providers },
-  }
+  };
 }
-export default Login
+export default Login;
