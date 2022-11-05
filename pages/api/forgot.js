73a853bc389a476f7db168596aa799665f3aa522
faizan <faizanzaid789@gmail.com>
2022-11-06 00:26:20 +0530

    added sending mail functionality

--- pages/api/forgot.js
+++ pages/api/forgot.js
@@ -1,6 +1,7 @@
 import { Forgot } from "../../backend/models/Forgot";
 import User from "../../backend/models/User";
 import connectDb from "../../backend/connect";
+import sgMail from "@sendgrid/mail";
 export default async function forgot(req, res) {
   if (req.method === "POST") {
     try {
@@ -17,10 +18,26 @@ export default async function forgot(req, res) {
           .json({ error: "User with this email do not exist!" });
 
       let addDetails = await Forgot.create({
-        token:req.body.token,
-        email:req.body.email
+        token: req.body.token,
+        email: req.body.email,
       });
       await addDetails.save();
+      sgMail.setApiKey(process.env.SENDGRID_KEY);
+      const msg = {
+        to: email, // Change to your recipient
+        from: "techwithfz@gmail.com", // Change to your verified sender
+        subject: "Your request to reset your password",
+        text: "and easy to do anywhere, even with Node.js",
+        html: `<div>
+        <p>Hi there, we've received a request to reset your password. Click the below link to reset your password.</p>
+        <a href=http://localhost:3000/forgot?authToken=${addDetails.token} target="_blank">Reset Password</a>
+        </div>`,
+      };
+      try {
+        await sgMail.send(msg);
+      } catch (er) {
+        return res.status(500).json({ error: "Internal server error" });
+      }
 
       res.json({ success: true });
     } catch (er) {
--- pages/forgot.tsx
+++ pages/forgot.tsx
@@ -4,6 +4,7 @@ import ReCAPTCHA from "react-google-recaptcha";
 import { toast } from "react-toastify";
 import { useSession } from "next-auth/react";
 import { getProviders, signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 
 interface Auth {
   callbackUrl: string;
@@ -23,43 +24,47 @@ type Props = {
 const token = process.env.NEXT_PUBLIC_FORGOT_TOKEN;
 const Forgot = (props: Props) => {
   const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const cpasswordRef = useRef<HTMLInputElement>(null);
   const [captcha, setCaptcha] = useState<string | null>("");
   const { data } = useSession();
+  const router = useRouter();
+  const { authToken } = router.query;
 
   const session: any = data;
   const handleSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault();
-    try {
-      if (!captcha) {
-        toast.error("Invalid captcha", {
-          position: "top-right",
-          autoClose: 1800,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "dark",
-        });
-        return;
-      }
-      const forgotPassword = await fetch(
-        "https://techwithfz.vercel.app/api/forgot",
-        {
-          method: "POST",
-          headers: {
-            "Content-type": "application/json",
-          },
-          body: JSON.stringify({
-            token,
-            email: emailRef.current?.value,
-          }),
+    if (authToken && authToken === token) {
+      try {
+        if (!captcha) {
+          toast.error("Invalid captcha", {
+            position: "top-right",
+            autoClose: 1800,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+          return;
         }
-      );
-      const response = await forgotPassword.json();
-      if (response.success && captcha) {
-        toast.success(
-          "Instructions have been sent to your email to reset your password!",
+        if (passwordRef?.current?.value !== cpasswordRef?.current?.value) {
+          toast.error("Passwords do not match!", {
+            position: "top-right",
+            autoClose: 1800,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+          return;
+        }
+      } catch (er) {
+        toast.error(
+          "There was an error while resetting your password. Please try again later!",
           {
             position: "top-right",
             autoClose: 1800,
@@ -71,8 +76,64 @@ const Forgot = (props: Props) => {
             theme: "dark",
           }
         );
-      } else {
-        toast.error("User with this email do not exist!", {
+      }
+    } else {
+      try {
+        if (!captcha) {
+          toast.error("Invalid captcha", {
+            position: "top-right",
+            autoClose: 1800,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+          return;
+        }
+        const forgotPassword = await fetch(
+          "https://techwithfz.vercel.app/api/forgot",
+          {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify({
+              token,
+              email: emailRef.current?.value,
+            }),
+          }
+        );
+        const response = await forgotPassword.json();
+        if (response.success && captcha) {
+          toast.success(
+            "Instructions have been sent to your email to reset your password!",
+            {
+              position: "top-right",
+              autoClose: 1800,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "dark",
+            }
+          );
+        } else {
+          toast.error("User with this email do not exist!", {
+            position: "top-right",
+            autoClose: 1800,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+        }
+      } catch (er) {
+        toast.error("Server error!", {
           position: "top-right",
           autoClose: 1800,
           hideProgressBar: false,
@@ -83,17 +144,6 @@ const Forgot = (props: Props) => {
           theme: "dark",
         });
       }
-    } catch (er) {
-      toast.error("Server error!", {
-        position: "top-right",
-        autoClose: 1800,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-      });
     }
   };
   function onChange(value: string | null) {
@@ -111,53 +161,119 @@ const Forgot = (props: Props) => {
           className="object-cover w-72 rounded-tl-md rounded-bl-md hidden md:inline-flex"
           alt="login_image"
         />
-
-        <div className="bg-[#1E1E1E] md:px-7 px-5 rounded-md md:rounded-none md:rounded-tr-md md:rounded-br-md flex flex-col gap-8  items-center  justify-center shadow-lg  w-full py-7 mt-28 md:mt-0 md:py-6 mb-12 md:mb-0 flex-1">
-          <form onSubmit={handleSubmit} className="flex flex-col gap-7 w-full">
-            <h2 className="font-bold text-2xl text-center text-white items-end">
-              Forgot Password?
-            </h2>
-
-            <div className="flex flex-col gap-1">
-              <label htmlFor="email" className="font-semibold text-white">
-                Email
-              </label>
-              <input
-                ref={emailRef}
-                type="email"
-                name="email"
-                id="email"
-                minLength={7}
-                autoComplete="false"
-                className="bg-[#2E2E2E] px-5 py-2 rounded-md outline-none text-white border-[#10935F] border-2 "
-                required
-                placeholder="Enter your email"
-                pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$"
-              />
-            </div>
-            <div className="flex justify-start w-full">
-              <ReCAPTCHA
-                sitekey="6LcGfNYiAAAAALXVdk9psJDgpo_nUEb6D5RdqW7T"
-                onChange={onChange}
-              />
-            </div>
-
-            <button
-              type="submit"
-              className={`${
-                session?.user || props.authState
-                  ? "bg-gray-500 text-white py-2 font-semibold rounded-md"
-                  : "commonButton py-2 font-semibold text-white"
-              } `}
-              disabled={session?.user || props.authState}
+        {authToken && authToken === token ? (
+          <div className="bg-[#1E1E1E] md:px-7 px-5 rounded-md md:rounded-none md:rounded-tr-md md:rounded-br-md flex flex-col gap-8  items-center  justify-center shadow-lg  w-full py-7 mt-28 md:mt-0 md:py-6 mb-12 md:mb-0 flex-1">
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col gap-7 w-full"
             >
-           Continue
-            </button>
-          </form>
-          <p className="text-white text-sm">
-            You'll receive an email from us to reset your password.
-          </p>
-        </div>
+              <h2 className="font-bold text-2xl text-center text-white items-end">
+                Reset Password
+              </h2>
+
+              <div className="flex flex-col gap-1">
+                <label htmlFor="password" className="font-semibold text-white">
+                  New Password
+                </label>
+                <input
+                  ref={passwordRef}
+                  type="password"
+                  name="password"
+                  id="password"
+                  minLength={7}
+                  autoComplete="false"
+                  className="bg-[#2E2E2E] px-5 py-2 rounded-md outline-none text-white border-[#10935F] border-2 "
+                  required
+                  placeholder="Enter your new password"
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <label htmlFor="cpassword" className="font-semibold text-white">
+                  Confirm new Password
+                </label>
+                <input
+                  ref={cpasswordRef}
+                  type="password"
+                  name="password"
+                  id="cpassword"
+                  minLength={7}
+                  autoComplete="false"
+                  className="bg-[#2E2E2E] px-5 py-2 rounded-md outline-none text-white border-[#10935F] border-2 "
+                  required
+                  placeholder="Confirm your password"
+                />
+              </div>
+              <div className="flex justify-start w-full">
+                <ReCAPTCHA
+                  sitekey="6LcGfNYiAAAAALXVdk9psJDgpo_nUEb6D5RdqW7T"
+                  onChange={onChange}
+                />
+              </div>
+
+              <button
+                type="submit"
+                className={`${
+                  session?.user || props.authState
+                    ? "bg-gray-500 text-white py-2 font-semibold rounded-md"
+                    : "commonButton py-2 font-semibold text-white"
+                } `}
+                disabled={session?.user || props.authState}
+              >
+                Continue
+              </button>
+            </form>
+          </div>
+        ) : (
+          <div className="bg-[#1E1E1E] md:px-7 px-5 rounded-md md:rounded-none md:rounded-tr-md md:rounded-br-md flex flex-col gap-8  items-center  justify-center shadow-lg  w-full py-7 mt-28 md:mt-0 md:py-6 mb-12 md:mb-0 flex-1">
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col gap-7 w-full"
+            >
+              <h2 className="font-bold text-2xl text-center text-white items-end">
+                Forgot Password?
+              </h2>
+
+              <div className="flex flex-col gap-1">
+                <label htmlFor="email" className="font-semibold text-white">
+                  Email
+                </label>
+                <input
+                  ref={emailRef}
+                  type="email"
+                  name="email"
+                  id="email"
+                  minLength={7}
+                  autoComplete="false"
+                  className="bg-[#2E2E2E] px-5 py-2 rounded-md outline-none text-white border-[#10935F] border-2 "
+                  required
+                  placeholder="Enter your email"
+                  pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$"
+                />
+              </div>
+              <div className="flex justify-start w-full">
+                <ReCAPTCHA
+                  sitekey="6LcGfNYiAAAAALXVdk9psJDgpo_nUEb6D5RdqW7T"
+                  onChange={onChange}
+                />
+              </div>
+
+              <button
+                type="submit"
+                className={`${
+                  session?.user || props.authState
+                    ? "bg-gray-500 text-white py-2 font-semibold rounded-md"
+                    : "commonButton py-2 font-semibold text-white"
+                } `}
+                disabled={session?.user || props.authState}
+              >
+                Continue
+              </button>
+            </form>
+            <p className="text-white text-sm">
+              You'll receive an email from us to reset your password.
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
