b440045e4afba7b569e04745ce1d8d5df7d07ae5
faizan <faizanzaid789@gmail.com>
2022-11-07 20:25:00 +0530

    Modified admin page

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -3,6 +3,9 @@ import { useRouter } from "next/router";
 import React, { useContext, useEffect, useRef, useState } from "react";
 import { toast } from "react-toastify";
 import { userContext } from "../context/userContext";
+import dynamic from "next/dynamic";
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+import "react-quill/dist/quill.snow.css";
 
 type Props = {};
 
@@ -13,7 +16,7 @@ const Admin = (props: Props) => {
   const titleRef = useRef<HTMLInputElement>(null);
   const authorRef = useRef<HTMLInputElement>(null);
   const slugRef = useRef<HTMLInputElement>(null);
-  const descriptionRef = useRef<HTMLTextAreaElement>(null);
+  const [value, setValue] = useState("");
 
   const router = useRouter();
 
@@ -23,8 +26,8 @@ const Admin = (props: Props) => {
 
   const handleSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault();
-    try{
-      const addPost = await fetch("https://techwithfz.vercel.app/api/addpost", {
+    try {
+      const addPost = await fetch("http://localhost:3000/api/addpost", {
         method: "POST",
         headers: {
           "Content-type": "application/json",
@@ -35,14 +38,14 @@ const Admin = (props: Props) => {
           blog: {
             title: titleRef.current?.value,
             author: authorRef.current?.value,
-            desc: descriptionRef.current?.value,
+            desc: value,
             slug: slugRef.current?.value,
           },
         }),
       });
-      const response=await addPost.json();
-      if(response.createdAt){
-        toast.success('Success while adding your post!', {
+      const response = await addPost.json();
+      if (response.createdAt) {
+        toast.success("Success while adding your post!", {
           position: "top-right",
           autoClose: 1800,
           hideProgressBar: false,
@@ -51,11 +54,10 @@ const Admin = (props: Props) => {
           draggable: true,
           progress: undefined,
           theme: "dark",
-          });
-          router.push("/blog")
-      }
-      else{
-        toast.error('You are not authorized!', {
+        });
+        router.push("/blog");
+      } else {
+        toast.error("You are not authorized!", {
           position: "top-right",
           autoClose: 1800,
           hideProgressBar: false,
@@ -64,11 +66,10 @@ const Admin = (props: Props) => {
           draggable: true,
           progress: undefined,
           theme: "dark",
-          });
+        });
       }
-    }
-    catch(er){
-      toast.error('An error occurred while posting your data!', {
+    } catch (er) {
+      toast.error("An error occurred while posting your data!", {
         position: "top-right",
         autoClose: 1800,
         hideProgressBar: false,
@@ -77,118 +78,111 @@ const Admin = (props: Props) => {
         draggable: true,
         progress: undefined,
         theme: "dark",
-        });
+      });
     }
-    
   };
 
   return (
     <div className="adminPage text-white">
       <Head>
-<title>Admins Only</title>
+        <title>Admins Only</title>
       </Head>
       <div className="max-w-[700px] mx-auto space-y-5 p-12">
-           <h2 className="font-semibold text-4xl text-white">Admin Panel </h2>
-           <div>
+        <h2 className="font-semibold text-4xl text-white">Admin Panel </h2>
+        <div>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-6">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="email">Email</label>
+              <input
+                ref={emailRef}
+                type="email"
+                name="email"
+                id="email"
+                minLength={7}
+                autoComplete="false"
+                className="bg-[#2E2E2E] px-5 py-3 rounded-md outline-none text-white border-[#10935F] border-2"
+                required
+                placeholder="Enter you admin email"
+                pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$"
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="password">Password</label>
+              <input
+                ref={passwordRef}
+                type="password"
+                name="password"
+                id="password"
+                autoComplete="false"
+                minLength={5}
+                className="bg-[#2E2E2E] px-5 py-3 rounded-md outline-none text-white border-[#10935F] border-2"
+                required
+                placeholder="Enter your password"
+              />
+            </div>
 
-      <form onSubmit={handleSubmit} className="flex flex-col gap-6">
-        <div className="flex flex-col gap-1">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="title">Title</label>
+              <input
+                ref={titleRef}
+                type="text"
+                name="title"
+                id="title"
+                autoComplete="false"
+                minLength={5}
+                className="bg-[#2E2E2E] px-5 py-3 rounded-md outline-none text-white border-[#10935F] border-2"
+                required
+                placeholder="Enter blog title"
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="author">Author</label>
+              <input
+                ref={authorRef}
+                type="text"
+                name="author"
+                id="author"
+                autoComplete="false"
+                minLength={5}
+                className="bg-[#2E2E2E] px-5 py-3 rounded-md outline-none text-white border-[#10935F] border-2"
+                required
+                placeholder="Author Name"
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="slug">Slug</label>
+              <input
+                ref={slugRef}
+                type="text"
+                name="slug"
+                id="slug"
+                autoComplete="false"
+                minLength={5}
+                className="bg-[#2E2E2E] px-5 py-3 rounded-md outline-none text-white border-[#10935F] border-2"
+                required
+                placeholder="how-to-add-slug"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <p>Description:</p>
+              <div className="bg-black !text-white border-[#10935F] border-2 rounded-md ">
+                <ReactQuill
+                  value={value}
+                  onChange={setValue}
+                  className="!text-white"
+                  
+                />
+              </div>
+            </div>
 
-        <label htmlFor="email">Email</label>
-        <input
-          ref={emailRef}
-          type="email"
-          name="email"
-          id="email"
-          minLength={7}
-          autoComplete="false"
-          className="bg-[#2E2E2E] px-5 py-3 rounded-md outline-none text-white border-[#10935F] border-2"
-          required
-          placeholder="Enter you admin email"
-          pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$"
-        />
+            <button
+              type="submit"
+              className="commonButton py-2 font-semibold text-white w-32"
+            >
+              Add Post
+            </button>
+          </form>
         </div>
-        <div className="flex flex-col gap-1">
-
-        <label htmlFor="password">Password</label>
-        <input
-          ref={passwordRef}
-          type="password"
-          name="password"
-          id="password"
-          autoComplete="false"
-          minLength={5}
-          className="bg-[#2E2E2E] px-5 py-3 rounded-md outline-none text-white border-[#10935F] border-2"
-          required
-          placeholder="Enter your password"
-        />
-
-        </div>
-
-        <div className="flex flex-col gap-1">
-        <label htmlFor="title">Title</label>
-        <input
-          ref={titleRef}
-          type="text"
-          name="title"
-          id="title"
-          autoComplete="false"
-          minLength={5}
-          className="bg-[#2E2E2E] px-5 py-3 rounded-md outline-none text-white border-[#10935F] border-2"
-          required
-          placeholder="Enter blog title"
-        />
-
-        </div>
-        <div className="flex flex-col gap-1">
-        <label htmlFor="author">Author</label>
-        <input
-          ref={authorRef}
-          type="text"
-          name="author"
-          id="author"
-          autoComplete="false"
-          minLength={5}
-          className="bg-[#2E2E2E] px-5 py-3 rounded-md outline-none text-white border-[#10935F] border-2"
-          required
-          placeholder="Author Name"
-        />
-
-        </div>
-        <div className="flex flex-col gap-1">
-
-        <label htmlFor="slug">Slug</label>
-        <input
-          ref={slugRef}
-          type="text"
-          name="slug"
-          id="slug"
-          autoComplete="false"
-          minLength={5}
-          className="bg-[#2E2E2E] px-5 py-3 rounded-md outline-none text-white border-[#10935F] border-2"
-          required
-          placeholder="how-to-add-slug"
-        />
-        </div>
-        <div className="flex flex-col gap-1">
-        <label htmlFor="description">Description</label>
-        <textarea
-          ref={descriptionRef}
-          name="description"
-          id="description"
-          autoComplete="false"
-          minLength={5}
-          className="bg-[#2E2E2E] px-5 py-3 rounded-md outline-none text-white border-[#10935F] border-2"
-          required
-          placeholder="Enter proper description"
-          rows={10}
-          style={{resize:"none"}}
-        />
-
-        </div>
-        <button type="submit" className='commonButton py-2 font-semibold text-white w-32'>Add Post</button>
-      </form>
-           </div>
       </div>
     </div>
   );
