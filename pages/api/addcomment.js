f8f4f3ce0f3b9c40c6ced3fe6a07c82c186cc0c5
faizan <faizanzaid789@gmail.com>
2022-11-09 00:04:12 +0530

    added some valid api end points to fetch comments.

---
+++ pages/api/addcomment.js
@@ -0,0 +1,38 @@
+import connectDb from "../../backend/connect";
+import User from "../../backend/models/User";
+import { Comment } from "../../backend/models/Comments";
+import { Blog } from "../../backend/models/Blog";
+export default async function addcomment(req, res) {
+  console.log(res.headers);
+  if (req.method === "POST") {
+    try {
+      let { comment, email, authToken, slug } = req.body;
+      await connectDb();
+      let user = await User.findOne({ email });
+      let postSlug = await Blog.findOne({ slug });
+
+      if (!user)
+        return res.status(401).json({ er: "You are not authenticated" });
+      if (!authToken)
+        return res.status(401), json({ er: "Login first to add a comment" });
+      if (!postSlug) return res.status(404).json({ err: "Not found" });
+
+      let newComment = await Comment.create({
+        comment,
+        slug,
+      });
+      await newComment.save();
+
+      let updatedPost = await Blog.findOneAndUpdate(
+        { slug: slug },
+        { $set: { userComments: [...postSlug.userComments, newComment] } },
+        { new: true }
+      );
+      res.send(updatedPost);
+    } catch (er) {
+      res.status(500).json({ err: er.message });
+    }
+  } else {
+    res.status(400).json({ err: "You are not authorized" });
+  }
+}
