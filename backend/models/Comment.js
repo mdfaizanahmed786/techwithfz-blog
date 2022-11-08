24750596d195fbf652b152f805b74de7bbe03d44
faizan <faizanzaid789@gmail.com>
2022-11-09 00:02:37 +0530

    added some api routes

---
+++ backend/models/Comment.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose"
+const CommentSchema=new mongoose.Schema({
+    comment:{type:String, date:Date.now(), immutable:true, required:true},
+    slug:{type:String, required:true}
+  })
+
+  let Comment=mongoose.models.Comment || mongoose.model("Comment", CommentSchema)
+
+export {Comment, CommentSchema};
---
+++ pages/api/getcomment.js
@@ -0,0 +1,29 @@
+import connectDb from "../../backend/connect";
+import {Comment} from "../../backend/models/Comments";
+import{ Blog }from "../../backend/models/Blog";
+export default async function getcomments(req,res){
+    if(req.method==="POST"){
+
+        try{
+        await connectDb();
+            const {slug}=req.body;
+            let allComments=await Blog.findOne({slug})
+            if(!allComments) return res.status(404).json({"error":"Not found"})
+            let all=[];
+            let allComment=[]
+            allComments.userComments.forEach((elem)=>all.push(elem.id))
+            for(let i of all){
+                let data=await Comment.findById(i);
+                allComment.push(data)
+        
+            }
+           res.json(allComment)
+        
+        }catch(er){
+        res.status(500).json({err:"Internal Server error"})
+        }
+    }
+    else{
+        res.status(401).json({"error":"You are not allowed to do so...."})
+    }
+}
