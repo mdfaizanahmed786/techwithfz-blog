fa8e33fbdbb129fe93c0b66a52718cb7caf1fe6e
faizan <faizanzaid789@gmail.com>
2022-11-26 16:26:26 +0530

    minor bug fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import React from "react";
 import HeroSection from "../components/HeroSection";
 import SinglePost from "../components/SinglePost";
 interface Response {
