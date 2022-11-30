8b4e72f152f7ee7c4f70ff901d4984d1ed572f60
faizan <faizanzaid789@gmail.com>
2022-11-30 15:42:20 +0530

    added an endpoint to like comment in api route.

---
+++ pages/api/likecomment.ts
@@ -0,0 +1,54 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Blog } from "../../backend/models/Blog";
+import connectDb from "../../backend/connect";
+type Reply = {
+  email: string;
+  reply: string;
+};
+interface UserComment {
+  id: string;
+  comment: string;
+  email: string;
+  slug: string;
+  createdAt: string;
+  replies: Reply[];
+  _v: number;
+}
+
+
+export default async function likecomment(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    try {
+      await connectDb();
+      const { id, slug, email } = req.body;
+      let postSlug = await Blog.findOne({ slug });
+      let comments=postSlug.userComments.filter((com:UserComment)=>com.id===id);
+
+      if (!postSlug) return res.status(404).json({ err: "Post not found" });
+
+    
+      if (comments.length===0) return res.status(404).json({ err: "Comment not found" });
+
+
+    
+      comments[0].likes.push(email);
+
+      postSlug.userComments.filter((com:UserComment)=>com.id===id);
+
+      await Blog.findOneAndUpdate(
+        { slug: slug },
+        { $set: { userComments: [...postSlug.userComments] } },
+        { new: true }
+      );
+
+      res.status(200).json({ success: true });
+    } catch (er: any) {
+      res.status(500).json({ error: "Internal server error", err: er.message });
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
