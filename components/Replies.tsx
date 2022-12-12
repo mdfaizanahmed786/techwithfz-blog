8671425e9e41e10bbbbcca949592fa4b3e097915
faizan <faizanzaid789@gmail.com>
2022-12-12 22:24:03 +0530

    few modifications in ui

--- components/Replies.tsx
+++ components/Replies.tsx
@@ -6,7 +6,7 @@ import { formatted_date } from "../lib/dateFormatter";
 function Replies({ reply, email, createdAt }: Reply) {
   return (
     <div className="bg-[#1e1e1e] px-5 py-5 rounded-md outline-none text-white border-[#10935F] border-2 flex flex-col gap-4 flex-1 ">
-      <div className="flex items-center gap-3">
+      <div className="flex items-center gap-3 flex-wrap">
         <FaUserCircle className="text-green-500" size={27} />
         <p className="font-bold">{email.replace("@gmail.com", "_gm")}</p>
         <p className='text-xs text-gray-300'>{formatted_date(createdAt)}</p>
