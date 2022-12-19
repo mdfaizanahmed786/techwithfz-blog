8a8a9ca2d06b52a08058f72500f724902cd0221d
faizan <faizanzaid789@gmail.com>
2022-12-19 15:52:31 +0530

    updated middleware route

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
-export { default } from "next-auth/middleware"
+
 
 
 export async function middleware(request: NextRequest, res: NextResponse) {
-  let cookie = request.cookies.get("authToken") as any;
+  let cookie = request.cookies.get("authToken");
 
   if (
-    cookie?.name === "authToken" &&
+    cookie &&
     (request.nextUrl.pathname.startsWith("/login") ||
       request.nextUrl.pathname.startsWith("/signup") || request.nextUrl.pathname.startsWith("/forgot")) 
       
@@ -14,7 +14,7 @@ export async function middleware(request: NextRequest, res: NextResponse) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
-if(request.nextUrl.pathname.startsWith("/admin") && cookie?.value!==process.env.NEXT_PUBLIC_ADMIN_TOKEN){
+if(request.nextUrl.pathname.startsWith("/admin") && cookie?.valueOf!==process.env.NEXT_PUBLIC_ADMIN_TOKEN){
   return NextResponse.redirect(new URL("/", request.url));
 }
 else{
