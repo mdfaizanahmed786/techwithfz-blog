d3d741dce0951a9eb8420404548491cf416601db
faizan <faizanzaid789@gmail.com>
2022-12-19 12:01:55 +0530

    added middleware file

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+export { default } from "next-auth/middleware"
+
+
+export async function middleware(request: NextRequest, res: NextResponse) {
+  let cookie = request.cookies.get("authToken");
+
+  if (
+    cookie?.name === "authToken" &&
+    (request.nextUrl.pathname.startsWith("/login") ||
+      request.nextUrl.pathname.startsWith("/signup") || request.nextUrl.pathname.startsWith("/forgot")) 
+      
+  ) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+if(request.nextUrl.pathname.startsWith("/admin") && cookie?.value!==process.env.NEXT_PUBLIC_ADMIN_TOKEN){
+  return NextResponse.redirect(new URL("/", request.url));
+}
+else{
+  return NextResponse.next();
+}
+                                 
+}
