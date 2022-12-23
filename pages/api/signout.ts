04cc737154f3a743c7e83fd6a3ce4bb4d68c5b97
faizan <faizanzaid789@gmail.com>
2022-12-23 20:31:29 +0530

    added some changes to logout api route.

--- pages/api/signout.ts
+++ pages/api/signout.ts
@@ -1,20 +1,30 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import cookie from 'cookie';
+import cookie, { serialize } from 'cookie';
 
 export default async function signout(req:NextApiRequest, res:NextApiResponse){
+const {cookies}=req;
+const jwt=cookies.authToken;
+if(!jwt){
+    return res.status(401).json({
+        error: 'Not authorized'
+    });
+}
+else{
+    const serialized=serialize('authToken', '', {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'strict',
+        maxAge: 0,
+        path: '/'
+    })
 
     res.setHeader('Set-Cookie', 
-        cookie.serialize(
-            'authToken', '', {
-                expires: new Date(0),
-                path: '/'
-            }
-        ),
-      
+    serialized
     );
 
    return res.status(200).json({
        success: 'Successfully logged out'
    });
+}
 
 }
