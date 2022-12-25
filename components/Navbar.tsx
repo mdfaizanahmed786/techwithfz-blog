e7b2655460cbc36bda3d28dc3442e7aa3ff14dd8
faizan <faizanzaid789@gmail.com>
2022-12-25 10:03:11 +0530

    test for signout route.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,40 +2,48 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useContext, useEffect, useState } from "react";
 import { userContext } from "../context/userContext";
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react";
 import { toast } from "react-toastify";
 
-
-
 const Navbar = () => {
-  const { cookieAuth, setCookieAuth} = useContext(userContext);
- const {isAdmin} =cookieAuth
-  const { data: session } = useSession()
+  const { cookieAuth, setCookieAuth } = useContext(userContext);
+  const { isAdmin } = cookieAuth;
+  const { data: session } = useSession();
   const router = useRouter();
- 
 
   const logOut = async () => {
-    router.push("/")
-    if(cookieAuth.email){
-    const removeUser=await fetch('https://techwithfz.vercel.app/api/signout')
-    const response=await removeUser.json();
-    if(response.success){
-      setCookieAuth("")
-    }
-  
-    toast.success('Logout Success!', {
-      position: "top-right",
-      autoClose: 1800,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "dark",
+    router.push("/");
+    if (cookieAuth.email) {
+      const removeUser = await fetch(
+        "https://techwithfz.vercel.app/api/signout",
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify({
+            key: "static_key",
+          }),
+        }
+      );
+      const response = await removeUser.json();
+      if (response.success) {
+        setCookieAuth("");
+      }
+
+      toast.success("Logout Success!", {
+        position: "top-right",
+        autoClose: 1800,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
       });
     }
-   
-    signOut()
+
+    signOut();
   };
   return (
     <header className="primary-bg sticky top-0 backdrop-blur-md z-20 shadow-md w-full">
@@ -64,7 +72,7 @@ const Navbar = () => {
         </div>
         {cookieAuth.email || session ? (
           <div className="flex items-center space-x-5">
-            <p  className="font-bold textStyle cursor-pointer">Welcome</p>
+            <p className="font-bold textStyle cursor-pointer">Welcome</p>
             <p
               className="commonButton font-semibold cursor-pointer text-white px-5 py-1"
               onClick={logOut}
@@ -72,16 +80,19 @@ const Navbar = () => {
               Logout
             </p>
             {isAdmin && (
-              <Link href={"/admin"} ><p className="commonButton hidden md:inline-flex font-semibold cursor-pointer text-white px-3 py-1">
-                Welcome Faizan
-              </p>
+              <Link href={"/admin"}>
+                <p className="commonButton hidden md:inline-flex font-semibold cursor-pointer text-white px-3 py-1">
+                  Welcome Faizan
+                </p>
               </Link>
             )}
           </div>
         ) : (
           <div className="flex items-center space-x-12">
             <Link href={"/login"}>
-              <p className="font-bold textStyle cursor-pointer hidden md:inline-flex">Login</p>
+              <p className="font-bold textStyle cursor-pointer hidden md:inline-flex">
+                Login
+              </p>
             </Link>
             <Link href={"/signup"}>
               <p className="commonButton font-semibold cursor-pointer text-white px-5 py-1">
--- pages/api/signout.ts
+++ pages/api/signout.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import cookie, { serialize } from 'cookie';
 
 export default async function signout(req:NextApiRequest, res:NextApiResponse){
+if (req.method !== 'POST') return res.status(405).json({ status: 'fail', message: 'Method not allowed here!' });
 const {cookies}=req;
 const jwt=cookies.authToken;
 if(!jwt){
@@ -9,7 +10,7 @@ if(!jwt){
         error: 'Not authorized'
     });
 }
-else{
+if (req.body.key === 'static_key') {
     const serialized=serialize('authToken', '', {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
@@ -21,10 +22,9 @@ else{
     res.setHeader('Set-Cookie', 
     serialized
     );
-
+}
    return res.status(200).json({
        success: 'Successfully logged out'
    });
 }
 
-}
