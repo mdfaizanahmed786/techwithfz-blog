c2a11a61be45175572e2bd58f84e7900d4c52007
faizan <faizanzaid789@gmail.com>
2022-12-26 19:17:32 +0530

    added categories array

---
+++ lib/categories.ts
@@ -0,0 +1,47 @@
+export const categories = [
+    {
+      id: 1,
+      category: "All",
+      isActive:true
+    },
+    {
+      id: 2,
+      category: "JavaScript",
+      isActive:false
+    },
+    {
+      id: 3,
+      category: "React",
+      isActive:false
+    },
+    {
+      id: 4,
+      category: "Node.js",
+      isActive:false
+    },
+    {
+      id: 5,
+      category: "Database",
+      isActive:false
+    },
+    {
+      id: 6,
+      category: "CSS",
+      isActive:false
+    },
+    {
+      id: 7,
+      category: "HTML",
+      isActive:false
+    },
+    {
+      id: 8,
+      category: "Technical Discussion",
+      isActive:false
+    },
+    {
+      id: 9,
+      category: "Others",
+      isActive:false
+    },
+  ];
