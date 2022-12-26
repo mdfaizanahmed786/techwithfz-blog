05066324c58336fb6783d48fd9825e8938d5a5b9
faizan <faizanzaid789@gmail.com>
2022-12-26 15:56:31 +0530

    added search functionality

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -5,9 +5,9 @@ import parse from "html-react-parser";
 import React, { useContext, useEffect, useState } from "react";
 import { AiOutlineClockCircle } from "react-icons/ai";
 import { toast } from "react-toastify";
-import jwt,{ JwtPayload, Secret } from "jsonwebtoken";
+import jwt, { JwtPayload, Secret } from "jsonwebtoken";
 import { userContext } from "../context/userContext";
-
+import { GoSearch } from "react-icons/go";
 
 interface Response {
   _id: string;
@@ -24,10 +24,9 @@ type Props = Response[] | any;
 
 const Blog = (props: Props) => {
   const { allBlogs, authCookie } = props;
-  const router = useRouter();
-  const [newPosts, setNewPosts]=useState(allBlogs)
+  const [newPosts, setNewPosts] = useState(allBlogs);
   const authContext = useContext(userContext);
-  const {isAdmin}=authCookie
+  const { isAdmin } = authCookie;
 
   useEffect(() => {
     if (authCookie) {
@@ -42,7 +41,7 @@ const Blog = (props: Props) => {
       await fetch(`https://techwithfz.vercel.app/api/deletepost/${id}`, {
         method: "DELETE",
       });
-      toast.success('Deleted your Post!', {
+      toast.success("Deleted your Post!", {
         position: "top-right",
         autoClose: 1800,
         hideProgressBar: false,
@@ -51,22 +50,49 @@ const Blog = (props: Props) => {
         draggable: true,
         progress: undefined,
         theme: "dark",
-        });
-
+      });
     }
   }
 
-  return (
+  const filterItems=(e: React.ChangeEvent<HTMLInputElement>)=>{
+    setNewPosts(
+      allBlogs.filter((post: Response) =>
+        post.title
+          .toLowerCase()
+          .includes(e.target.value.toLowerCase())
+      )
+    )
 
-    <div className="bg-[#2E2E2E] w-full">
+  }
+
+  return (
+    <div className=" bg-[#2E2E2E]">
+    <div className="bg-[#2E2E2E] w-full h-full">
       <Head>
         <title>TechWithFZ - Blog</title>
       </Head>
       <h1 className="text-3xl text-white font-bold text-center py-5 ">
         All Blogs({newPosts.length})
       </h1>
+
+      <div>
+        <div className="searchContainer py-6 md:max-w-[1030px] md:mx-auto mx-4 space-y-3">
+          <p className="font-bold textStyle">Search</p>
+          <div className="bg-[#1E1E1E] flex  text-white ring-[#10935F] ring-2 rounded-md">
+            <input
+              type="text"
+              className="searchInput flex-1 bg-transparent  focus:outline-none py-2 px-5"
+              onChange={(e) =>filterItems(e)
+              }
+              placeholder="Search for a post..."
+            />
+            <div className="bg-[#10B45B] px-5 py-4 rounded-br-md rounded-tr-md ">
+              <GoSearch size={20} />
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="flex flex-col gap-7 py-6 md:max-w-[1030px] md:mx-auto mx-4">
-     
         {newPosts.map((blog: Response) => (
           <div
             key={blog._id}
@@ -79,8 +105,8 @@ const Blog = (props: Props) => {
               <Link href={`blogpost/${blog.slug}`}>{blog.title}</Link>
             </h2>
             <div className="">
-            <p className="info text-white line-clamp-2">{parse(blog.desc)}</p>
-          </div>
+              <p className="info text-white line-clamp-2">{parse(blog.desc)}</p>
+            </div>
             <div className="flex justify-between">
               <div className="flex gap-5">
                 <Link href={`blogpost/${blog.slug}`}>
@@ -89,7 +115,10 @@ const Blog = (props: Props) => {
                   </button>
                 </Link>
                 {isAdmin && (
-                  <button className="commonButton py-2 hidden md:block w-32 font-semibold text-white" onClick={()=>deletePost(blog._id)}>
+                  <button
+                    className="commonButton py-2 hidden md:block w-32 font-semibold text-white"
+                    onClick={() => deletePost(blog._id)}
+                  >
                     Delete Post
                   </button>
                 )}
@@ -103,6 +132,7 @@ const Blog = (props: Props) => {
         ))}
       </div>
     </div>
+    </div>
   );
 };
 
@@ -112,14 +142,12 @@ export async function getServerSideProps(context: any) {
   let authCookie: string | JwtPayload = "";
   if (context?.req?.cookies["authToken"]) {
     let result = context?.req?.cookies["authToken"];
-    if(result===process.env.NEXT_PUBLIC_ADMIN_TOKEN){
+    if (result === process.env.NEXT_PUBLIC_ADMIN_TOKEN) {
       authCookie = {
-        email:'admin',
+        email: "admin",
         isAdmin: true,
       };
-    }
-    else{
-
+    } else {
       authCookie = jwt.verify(result, process.env.JWT_SECRET as Secret);
     }
   }
