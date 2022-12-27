26c90d8237dec1cbd91ad6719ac6ea6109272260
faizan <faizanzaid789@gmail.com>
2022-12-27 08:47:04 +0530

    added some drop shadow in categories section.

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -8,7 +8,7 @@ type Props = {
 const Categories = ({ category, isActive }: Props) => {
   return (
     <div
-      className={`category bg-[#1E1E1E] px-4 py-2 rounded-md text-white cursor-pointer text-sm ${
+      className={`category bg-[#1E1E1E] px-4 py-2 rounded-md text-white cursor-pointer text-sm drop-shadow-md ${
         isActive && "ring-[#10935F] ring-2"
       }`}
     >
