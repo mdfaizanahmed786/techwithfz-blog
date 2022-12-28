c697e42e835766c2d573bc93c0e1096985ac0b77
faizan <faizanzaid789@gmail.com>
2022-12-28 09:47:04 +0530

    added some improvements in admin page.

--- pages/blogpost/[slug].tsx
+++ pages/blogpost/[slug].tsx
@@ -78,7 +78,7 @@ const slug = (props: Response | any) => {
     e.preventDefault();
 
     const comment = await fetch(
-      "https://techwithfz.vercel.app/api/addcomment",
+      "http://localhost:3000/api/addcomment",
       {
         method: "POST",
         headers: {
@@ -234,7 +234,7 @@ const slug = (props: Response | any) => {
 
 export async function getServerSideProps(context: any) {
   const { params } = context;
-  const response = await fetch("https://techwithfz.vercel.app/api/getposts");
+  const response = await fetch("http://localhost:3000/api/getposts");
   const { allBlogs } = await response.json();
   let specificPost = allBlogs.filter((blog: Response) => {
     return blog.slug === params.slug;
