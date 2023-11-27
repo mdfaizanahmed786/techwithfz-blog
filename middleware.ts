f6c008b6560e2fde2565ec3fc4cec60a0d66b946
Mohammed Faizan Ahmed <85175130+mdfaizanahmed786@users.noreply.github.com>
2023-11-28 04:23:35 +0530

    test: again of config folder

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
-export const config = {
-  matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
-}
 
 
 
 export async function middleware(request: NextRequest, res: NextResponse) {
   let cookie = request.cookies.get("authToken");
   
-
+  
   if (
     cookie &&
     (request.nextUrl.pathname.startsWith("/login") ||
       request.nextUrl.pathname.startsWith("/signup") || request.nextUrl.pathname.startsWith("/forgot")) 
       
-  ) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
-if(request.nextUrl.pathname.startsWith("/admin") && cookie!==process.env.NEXT_PUBLIC_ADMIN_TOKEN){
-  return NextResponse.redirect(new URL("/", request.url));
-}
-else{
-  return NextResponse.next();
-}
-                                 
-}
+      ) {
+        return NextResponse.redirect(new URL("/", request.url));
+      }
+      
+      if(request.nextUrl.pathname.startsWith("/admin") && cookie!==process.env.NEXT_PUBLIC_ADMIN_TOKEN){
+        return NextResponse.redirect(new URL("/", request.url));
+      }
+      else{
+        return NextResponse.next();
+      }
+      
+    }
+    export const config = {
+      matcher: [
+        '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      ],
+    }
