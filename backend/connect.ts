8d1a49036b184509c78e9e052109d180ba4b0a48
Mohammed Faizan Ahmed <85175130+mdfaizanahmed786@users.noreply.github.com>
2024-04-12 12:44:51 +0530

    Update connect.ts

--- backend/connect.ts
+++ backend/connect.ts
@@ -1,8 +1,25 @@
 import mongoose from "mongoose";
 
+type ConnectionState={
+  connectionReady: number
+}
+const connection:ConnectionState={}
+const connectDb = async () : Promise<void> => {
+    if(connection.connectionReady){
+      console.log("Already connected database");
+      return;
+    }
+  try{
+    const db=await mongoose.connect(process.env.MONGO_URI || '', {});
+    connection.connectionReady=db.connections[0].readyState;
 
-const connectDb = async () => {
-  mongoose.connect(process.env.MONGO_URI as string)
+    console.log("connected");
+  }
+
+  catch(error){
+    console.log(error)
+    process.exit(1);
+  }
 };
 
 export default connectDb;
